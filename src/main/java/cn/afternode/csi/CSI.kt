1a6af36e7e39c205ec6d302aa168e4bf332a5d3f
H3xadecimal <h3xadecimal@afternode.cn>
2023-10-03 16:34:14 +0800

    Exception handler

--- src/main/java/cn/afternode/csi/CSI.kt
+++ src/main/java/cn/afternode/csi/CSI.kt
@@ -6,9 +6,13 @@ import cn.afternode.csi.installer.impl.Mohist
 import cn.afternode.csi.installer.impl.Paper
 import cn.afternode.csi.ui.MainWindow
 import cn.afternode.csi.utils.Configuration
+import com.google.gson.GsonBuilder
+import javax.swing.JOptionPane
 import kotlin.system.exitProcess
 
 object CSI {
+    @JvmStatic val GSON = GsonBuilder().setPrettyPrinting().create()
+
     lateinit var config: Configuration
         private set
     lateinit var fileManager: FileManager
@@ -20,19 +24,33 @@ object CSI {
         private set
 
     fun launch(args: Array<String>) {
-        fileManager = FileManager()
-        installerManager = InstallerManager()
-            .register(Paper())
-            .register(Mohist())
+        try {
+            fileManager = FileManager()
+            installerManager = InstallerManager()
+                .register(Paper())
+                .register(Mohist())
 
-        config = fileManager.loadConfig()
+            config = fileManager.loadConfig()
 
-        mainWindow = MainWindow()
-        mainWindow.isVisible = true
+            mainWindow = MainWindow()
+            mainWindow.isVisible = true
+        } catch (t: Throwable) {
+            JOptionPane.showMessageDialog(null, "Critical error in main process: ${t.stackTraceToString()}", "CSI", JOptionPane.ERROR_MESSAGE)
+            handleExit(1)
+        }
+    }
+
+    fun handleExit(status: Int) {
+        try {
+            FileManager.configFile.writeText(GSON.toJson(config), Charsets.UTF_8)
+        } catch (t: Throwable) {
+            t.printStackTrace()
+        }
+
+        exitProcess(status)
     }
 
     fun handleExit() {
-
-        exitProcess(0)
+        handleExit(0)
     }
 }
